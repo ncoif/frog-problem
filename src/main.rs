5e8374f1dd26bf464daa024499292ab4b27bfa17
ncoif <github@ncoif.net>
2019-09-17 21:23:16 +0100

    Extract random.rs for easier unit tests

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
 extern crate rand;
 
-use rand::rngs::ThreadRng;
-use rand::Rng;
+mod random;
+
+use random::Random;
+use random::RandomTrait;
 
 static SIZE: u8 = 10;
 static TOTAL_RUNS: u32 = 100_000;
 
 fn main() {
-    let mut random_generator = rand::thread_rng();
+    let mut rnd = Random::new();
+
     let (average, _): (f64, u64) = (0..TOTAL_RUNS)
-        .map(|_| frog_run(SIZE, &mut random_generator))
+        .map(|_| frog_run(&mut rnd, SIZE))
         .map(u64::from)
         .fold((0., 0), |(average, count), x| {
             (
@@ -21,23 +24,15 @@ fn main() {
 }
 
 // return the number of hops for a single run
-fn frog_run(size: u8, random_generator: &mut ThreadRng) -> u8 {
+fn frog_run(rnd: &mut dyn RandomTrait, size: u8) -> u8 {
     let mut remaining_distance = size;
 
     let mut number_hops = 0;
     while remaining_distance > 0 {
-        let next_hop = gen_range(1, remaining_distance, random_generator);
+        let next_hop = rnd.gen_range(1, remaining_distance);
         number_hops += 1;
         remaining_distance -= next_hop;
     }
 
     number_hops
-}
-
-fn gen_range(low: u8, high: u8, random_generator: &mut ThreadRng) -> u8 {
-    if low == high {
-        low
-    } else {
-        random_generator.gen_range(low, high)
-    }
 }
---
+++ src/random.rs
@@ -0,0 +1,23 @@
+use rand::Rng;
+
+pub trait RandomTrait {
+    fn gen_range(&mut self, low: u8, high: u8) -> u8;
+}
+
+pub struct Random {}
+
+impl Random {
+    pub fn new() -> Self {
+        Random {}
+    }
+}
+
+impl RandomTrait for Random {
+    fn gen_range(&mut self, low: u8, high: u8) -> u8 {
+        if low == high {
+            low
+        } else {
+            rand::thread_rng().gen_range(low, high)
+        }
+    }
+}
